cf8dab7fa9399f76ff7b79c3f43f5fbb7b8fe5da
IdsRehorst <idsrehorst@gmail.com>
2025-05-26 15:48:52 +0200

    Script to execute benchmarks on delftblue

---
+++ run_delftblue.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+#SBATCH --job-name="tri_solve_benchmark"
+#SBATCH --partition=compute-p1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH --time=01:00:00
+#SBATCH --exclusive
+#SBATCH --mem=0
+#SBATCH --account=research-eemcs-diam
+
+export OMP_PROC_BIND=true
+export OMP_PLACES=cores
+
+export OMP_NUM_THREADS=1
+
+srun --cpu-bind=ldoms ./cmake-build-debug-docker/tri_solve
