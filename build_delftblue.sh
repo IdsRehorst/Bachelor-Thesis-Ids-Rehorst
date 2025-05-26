1971e611c0b2a1d2466a17742af107e51ce57cf3
IdsRehorst <56934835+IdsRehorst@users.noreply.github.com>
2025-05-26 16:09:49 +0200

    Load hwloc module

--- build_delftblue.sh
+++ build_delftblue.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-#
-#SBATCH --job-name="Build Project"
-#SBATCH --partition=compute-p1
-#SBATCH --time=01:00:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1G
-#SBATCH --account=research-eemcs-diam
 
 module load intel/oneapi-all
-module load 2023r1-intel
+module load 2023r1-gcc11
 module load cmake
+module load hwloc
 
-srun ./cmake/build_delftblue.bash
+echo $HWLOC_ROOT
+
+./cmake/build_delftblue.bash
