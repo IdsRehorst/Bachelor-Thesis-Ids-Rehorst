2e51276b68694beeb42abc5d6d16e7fcde02660f
IdsRehorst <56934835+IdsRehorst@users.noreply.github.com>
2025-07-03 01:14:12 +0200

    Build and run instructions for DelftBlue

--- build_delftblue.sh
+++ build_delftblue.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+
+module load 2024r1
 module load intel/oneapi-all
-module load 2023r1-gcc11
 module load cmake
 module load hwloc
 
-echo $HWLOC_ROOT
+module use /scratch/iwdhrehorst/spack/share/spack/lmod/linux-rhel8-x86_64/Core
+module load gcc/15.1.0
+
+module use /home/iwdhrehorst/spack/share/spack/lmod/linux-rhel8-x86_64/Core/
+module load kokkos/4.6.01
+module load kokkos-kernels/4.6.01
+
+export LIKWID_ROOT=$HOME/likwid-5.4
+export TBB_ROOT=/beegfs/apps/generic/intel/oneapi_2022.3/tbb/latest
 
 ./cmake/build_delftblue.bash
--- run_delftblue.sh
+++ run_delftblue.sh
@@ -5,15 +5,22 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
-#SBATCH --time=01:00:00
+#SBATCH --time=04:00:00
 #SBATCH --exclusive
 #SBATCH --mem=0
 #SBATCH --account=research-eemcs-diam
 
-module load 2023r1-gcc11       
-module load intel/oneapi-all     
-module load hwloc
+module load 2024r1
+module load intel/oneapi-all
 module load cmake
+module load hwloc
+
+module use /scratch/iwdhrehorst/spack/share/spack/lmod/linux-rhel8-x86_64/Core
+module load gcc/15.1.0
+
+module use /home/iwdhrehorst/spack/share/spack/lmod/linux-rhel8-x86_64/Core/
+module load kokkos/4.6.01
+module load kokkos-kernels/4.6.01
 
 # Included to stop runtime issues, you have to change the name of the user here if you want to run it yourself
 # This should be implemented in the CMakelist in the future.
@@ -23,6 +30,7 @@ export LD_LIBRARY_PATH="$RACE_LIB:$LD_LIBRARY_PATH"
 export OMP_PROC_BIND=true
 export OMP_PLACES=cores
 
-export OMP_NUM_THREADS=12  
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
 
-srun --cpu-bind=ldoms ./cmake-build-debug-docker/tri_solve
+srun --cpu-bind=ldoms ./cmake-build-debug-docker/tri_solve benchmark_1.csv
